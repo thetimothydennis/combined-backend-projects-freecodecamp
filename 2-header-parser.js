74146e6112378be9f09b9e529b921cd09b82dc62
Timothy Dennis <timothyddennis@gmail.com>
2023-05-29 21:42:57 -0500

    fix: fixed one wrong value in 2-header-parser.js

--- 2-header-parser.js
+++ 2-header-parser.js
@@ -1,7 +1,7 @@
 
 module.exports.whoAmI = function whoAmI (req) {
     let responseObj = {}
-    responseObj.ipAddr = req.header("x-forwarded-for");
+    responseObj.ipaddress = req.header("x-forwarded-for");
     responseObj.language = req.header("accept-language");
     responseObj.software = req.header("user-agent");
     return responseObj
